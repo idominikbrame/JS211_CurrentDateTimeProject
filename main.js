4e24a9c34f254f62fb08f8770b0f7d93af40bee4
Dominik Brame <idominikbrame@Dominiks-MacBook-Pro.local>
2021-02-24 12:19:41 -0600

    added user input option

--- main.js
+++ main.js
@@ -15,11 +15,20 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
-
+function num2string() {
+  let convertThis = document.querySelector("#number").value; 
+  convertThis.toString();
+  document.querySelector("#converted").innerHTML= "Your string is: " + "'" + convertThis + "'";
+}
 
 
 // Write a JavaScript program to convert a string to the number.
 
+function string2num() {
+  let convertThis = document.querySelector("#numberString").value; 
+  Number.convertThis;
+  document.querySelector("#convertedNumber").innerHTML= "Your number is: " + convertThis;
+}
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -29,22 +38,80 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
-  
+  function whichType() {
+    let typeinput = document.getElementById("whichType").value;
+    let result = document.getElementById("showType")
 
+    if(parseInt(typeinput) == Number) {
+      result.innerHTML = "Number";
+    } else if (typeinput.toLowerCase() == "true" || typeinput.toLowerCase() == "false"){
+      result.innerHTML = "Boolean";
+    } else if (parseInt(typeinput) == NaN) {
+      result.innerHTML = "NaN or Null";
+    } else if (typeinput == "") {
+      result.innerHTML = "undefined";
+    } else {
+      result.innerHTML = "string";
+    }
+  }
+ 
   
 // Write a JavaScript program that adds 2 numbers together.
 
-
+function aPlusB () {
+  let numA = document.querySelector("#numberA").value;
+  let numB = document.querySelector("#numberB").value;
+  let result = parseInt(numA) + parseInt(numB);
+  document.querySelector("#result").innerHTML = result;
+}
 
 // Write a JavaScript program that runs only when 2 things are true.
-
+function truthy() {
+  let truthy1 = document.querySelector("#truthyOrNah1").value;
+  let truthy2 = document.querySelector("#truthyOrNah2").value;
+  if (parseInt(truthy2) && parseInt(truthy1)) {
+    document.querySelector("#isTruthy").innerHTML = "SUCCESS"
+  } else if (truthy1 !==  "false" && truthy2 !== "false" && truthy1 !== "0" && truthy2 !== "0"){
+    document.querySelector("#isTruthy").innerHTML = "SUCCESS"
+  }  else {
+    alert("Please enter two truthy values")
+  }
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
-
+function oneTruthy() {
+  let truthy1 = document.querySelector("#oneTruthy1").value;
+  let truthy2 = document.querySelector("#oneTruthy2").value;
+  if (parseInt(truthy2) && parseInt(truthy1)) {
+    document.querySelector("#isOneTruthy").innerHTML = "SUCCESS"
+  } else if (truthy1 == "false" || truthy2 == "false"){
+    alert("Please enter at least one truthy value")
+  } else if (truthy1 === "0" || truthy2 === "0"){
+    alert("Please enter at least one truthy value")
+  }  
+  else {
+    document.querySelector("#isOneTruthy").innerHTML = "SUCCESS"
+  }
+}
 
 // Write a JavaScript program that runs when both things are not true.  
+function noTruthy() {
+  let truthy1 = document.querySelector("#noTruthy1").value;
+  let truthy2 = document.querySelector("#noTruthy2").value;
+  if (parseInt(truthy2) && parseInt(truthy1)) {
+    alert("Please enter no truthy values")
+  } else if (truthy1 === "false" && truthy2 === "false"){
+    document.querySelector("#isNoTruthy").innerHTML = "NO TRUTHY"
+  } else if (truthy1 === "0" && truthy2 === "0"){
+    document.querySelector("#isNoTruthy").innerHTML = "NO TRUTHY"
+  }  
+  else {
+    document.querySelector("#isNoTruthy").innerHTML = "NO TRUTHY"
+  }
+}
+
 
 // ***************************
 //         PART TWO
@@ -58,7 +125,13 @@ const displayDate = () => {
 // 6. go to `index.html` 
 // 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
 
-
+whichType(true);
+whichType(false);
+whichType("sat");
+whichType(11);
+whichType(null);
+whichType(NaN);
+whichType(undefined)
 
 
 // Additional Resources
